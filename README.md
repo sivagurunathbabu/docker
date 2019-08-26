f86b9379748b03cab8e93ad945338d6d79d8e94b
sivagurunathbabu <sivagurunathbabu@gmail.com>
2019-08-26 18:35:46 +0530

    Refactoring changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# docker
-Repo for docker
--- src/test/java/com/demo/tracker/UserTimeTrackerApplicationTests.java
+++ src/test/java/com/demo/tracker/UserTimeTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.school;
+package com.demo.tracker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TimeTrackerApplicationTests {
+public class UserTimeTrackerApplicationTests {
 
 	@Test
 	public void contextLoads() {
